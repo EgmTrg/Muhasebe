17e04782c7b16905534ae7a3882336f9b1229126
EgmTrg <e.turoglu@hotmail.com>
2021-04-13 14:11:51 +0300

    Created Tools Class

---
+++ Muhasebe/Muhasebe.ORM/Tools.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Muhasebe.ORM
+{
+    public static class Tools
+    {
+        private static SqlConnection connection;
+
+        public static SqlConnection Connection
+        {
+            get
+            {
+                return connection ?? new SqlConnection(ConfigurationManager.ConnectionStrings["MuhasebeConnection_Local"].ConnectionString);
+            }
+            set { connection = value; }
+        }
+
+        public static bool ExecuteQuerry(SqlCommand cmd)
+        {
+            try
+            {
+                if (cmd.Connection.State == System.Data.ConnectionState.Closed)
+                    cmd.Connection.Open();
+                int affected = cmd.ExecuteNonQuery();
+                return affected > 0 ? true : false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                if (cmd.Connection.State == System.Data.ConnectionState.Open)
+                    cmd.Connection.Close();
+            }
+        }
+
+    }
+}
