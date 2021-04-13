03fd4f8e402dd05d74a6097310846a1765c91e96
EgmTrg <e.turoglu@hotmail.com>
2021-04-13 14:14:51 +0300

    Added Select Querry for all Tables

--- Muhasebe/Muhasebe.ORM/ORMBase.cs
+++ Muhasebe/Muhasebe.ORM/ORMBase.cs
@@ -1,27 +1,38 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Muhasebe.ORM
 {
     public class ORMBase<Table> : IORM<Table> where Table : class
     {
+        public Type myVariableType { get { return typeof(Table); } }
         public DataTable Select()
         {
-            throw new System.NotImplementedException();
+            DataTable dt = new DataTable();
+            SqlDataAdapter adp = new SqlDataAdapter();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = Tools.Connection;
+            cmd.CommandText = $"Select_{myVariableType.Name}";
+            cmd.CommandType = CommandType.StoredProcedure;
+            adp.SelectCommand = cmd;
+            adp.Fill(dt);
+            return dt;
         }
 
         public bool Insert(Table t)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public bool Update(Table t)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
-                
+
         public bool Delete(int id)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
     }
