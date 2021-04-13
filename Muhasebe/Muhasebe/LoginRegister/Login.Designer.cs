5bc5aca7de9eb58eaa1f7bdd0c0812ec8be0679b
EgmTrg <e.turoglu@hotmail.com>
2021-04-13 13:41:09 +0300

    Added Buttons

--- Muhasebe/Muhasebe/LoginRegister/Login.Designer.cs
+++ Muhasebe/Muhasebe/LoginRegister/Login.Designer.cs
@@ -30,20 +30,24 @@ namespace Muhasebe.LoginRegister
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.username_textBox = new System.Windows.Forms.TextBox();
-            this.password_maskedTextBox = new System.Windows.Forms.MaskedTextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.login_button = new System.Windows.Forms.Button();
+            this.register_button = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.password_maskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.username_textBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
-            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
             this.lineShape2 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.login_button);
+            this.panel1.Controls.Add(this.register_button);
             this.panel1.Controls.Add(this.checkBox1);
             this.panel1.Controls.Add(this.password_maskedTextBox);
             this.panel1.Controls.Add(this.username_textBox);
@@ -53,28 +57,53 @@ namespace Muhasebe.LoginRegister
             this.panel1.Controls.Add(this.shapeContainer1);
             this.panel1.Location = new System.Drawing.Point(40, 19);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(322, 247);
+            this.panel1.Size = new System.Drawing.Size(322, 294);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // login_button
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(3, 84);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 22);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Username";
+            this.login_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(70)))), ((int)(((byte)(83)))));
+            this.login_button.FlatAppearance.BorderColor = System.Drawing.SystemColors.ControlLight;
+            this.login_button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(70)))), ((int)(((byte)(83)))));
+            this.login_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(167)))), ((int)(((byte)(153)))));
+            this.login_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.login_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.login_button.ForeColor = System.Drawing.Color.Snow;
+            this.login_button.Location = new System.Drawing.Point(170, 242);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(138, 40);
+            this.login_button.TabIndex = 4;
+            this.login_button.Text = "Login";
+            this.login_button.UseVisualStyleBackColor = false;
+            this.login_button.Click += new System.EventHandler(this.login_button_Click);
             // 
-            // username_textBox
+            // register_button
             // 
-            this.username_textBox.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.username_textBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.username_textBox.Font = new System.Drawing.Font("Century Gothic", 14.25F);
-            this.username_textBox.Location = new System.Drawing.Point(7, 109);
-            this.username_textBox.Name = "username_textBox";
-            this.username_textBox.Size = new System.Drawing.Size(298, 24);
-            this.username_textBox.TabIndex = 1;
+            this.register_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(70)))), ((int)(((byte)(83)))));
+            this.register_button.FlatAppearance.BorderColor = System.Drawing.SystemColors.ControlLight;
+            this.register_button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(70)))), ((int)(((byte)(83)))));
+            this.register_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(167)))), ((int)(((byte)(153)))));
+            this.register_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.register_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.register_button.ForeColor = System.Drawing.Color.Snow;
+            this.register_button.Location = new System.Drawing.Point(7, 242);
+            this.register_button.Name = "register_button";
+            this.register_button.Size = new System.Drawing.Size(138, 40);
+            this.register_button.TabIndex = 3;
+            this.register_button.Text = "Register";
+            this.register_button.UseVisualStyleBackColor = false;
+            this.register_button.Click += new System.EventHandler(this.register_button_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox1.Location = new System.Drawing.Point(7, 216);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(199, 20);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Don\'t ask again. Remember Me!";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // password_maskedTextBox
             // 
@@ -84,17 +113,20 @@ namespace Muhasebe.LoginRegister
             this.password_maskedTextBox.Location = new System.Drawing.Point(7, 179);
             this.password_maskedTextBox.Name = "password_maskedTextBox";
             this.password_maskedTextBox.Size = new System.Drawing.Size(298, 24);
-            this.password_maskedTextBox.TabIndex = 2;
+            this.password_maskedTextBox.TabIndex = 1;
+            this.password_maskedTextBox.UseSystemPasswordChar = true;
+            this.password_maskedTextBox.Enter += new System.EventHandler(this.password_maskedTextBox_Enter);
             // 
-            // label2
+            // username_textBox
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(104, 15);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(102, 39);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Login";
+            this.username_textBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.username_textBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.username_textBox.Font = new System.Drawing.Font("Century Gothic", 14.25F);
+            this.username_textBox.Location = new System.Drawing.Point(7, 109);
+            this.username_textBox.Name = "username_textBox";
+            this.username_textBox.Size = new System.Drawing.Size(298, 24);
+            this.username_textBox.TabIndex = 0;
+            this.username_textBox.Enter += new System.EventHandler(this.username_textBox_Enter);
             // 
             // label3
             // 
@@ -106,16 +138,25 @@ namespace Muhasebe.LoginRegister
             this.label3.TabIndex = 0;
             this.label3.Text = "Password";
             // 
-            // checkBox1
+            // label2
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.checkBox1.Location = new System.Drawing.Point(7, 216);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(199, 20);
-            this.checkBox1.TabIndex = 3;
-            this.checkBox1.Text = "Don\'t ask again. Remember Me!";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(104, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 39);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Login";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(3, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
             // 
             // shapeContainer1
             // 
@@ -125,17 +166,9 @@ namespace Muhasebe.LoginRegister
             this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
             this.lineShape2,
             this.lineShape1});
-            this.shapeContainer1.Size = new System.Drawing.Size(322, 247);
+            this.shapeContainer1.Size = new System.Drawing.Size(322, 294);
             this.shapeContainer1.TabIndex = 4;
             this.shapeContainer1.TabStop = false;
-            // 
-            // lineShape1
-            // 
-            this.lineShape1.Name = "lineShape1";
-            this.lineShape1.X1 = 5;
-            this.lineShape1.X2 = 306;
-            this.lineShape1.Y1 = 133;
-            this.lineShape1.Y2 = 133;
             // 
             // lineShape2
             // 
@@ -145,16 +178,24 @@ namespace Muhasebe.LoginRegister
             this.lineShape2.Y1 = 203;
             this.lineShape2.Y2 = 203;
             // 
+            // lineShape1
+            // 
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 5;
+            this.lineShape1.X2 = 306;
+            this.lineShape1.Y1 = 133;
+            this.lineShape1.Y2 = 133;
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ClientSize = new System.Drawing.Size(399, 298);
+            this.ClientSize = new System.Drawing.Size(399, 338);
             this.Controls.Add(this.panel1);
             this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
-            this.Load += new System.EventHandler(this.Login_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -173,5 +214,7 @@ namespace Muhasebe.LoginRegister
         private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
         private Microsoft.VisualBasic.PowerPacks.LineShape lineShape2;
         private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private System.Windows.Forms.Button login_button;
+        private System.Windows.Forms.Button register_button;
     }
 }
--- Muhasebe/Muhasebe/LoginRegister/Login.cs
+++ Muhasebe/Muhasebe/LoginRegister/Login.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System.Data;
+using System.Windows.Forms;
 
 namespace Muhasebe.LoginRegister
 {
@@ -9,9 +10,31 @@ namespace Muhasebe.LoginRegister
             InitializeComponent();
         }
 
-        private void Login_Load(object sender, System.EventArgs e)
+        #region Some Properties
+        private void username_textBox_Enter(object sender, System.EventArgs e)
+        {
+            username_textBox.SelectAll();
+        }
+
+        private void password_maskedTextBox_Enter(object sender, System.EventArgs e)
+        {
+            password_maskedTextBox.SelectAll();
+        }
+        #endregion
+
+        private void login_button_Click(object sender, System.EventArgs e)
         {
             
+
+            if (checkBox1.Checked) 
+            {
+                // do remember stuffs
+            }
+        }
+
+        private void register_button_Click(object sender, System.EventArgs e)
+        {
+            //new Regsiter().ShowDialog();
         }
     }
 }
