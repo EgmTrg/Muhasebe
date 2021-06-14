a86544cb2000ab6dd3e7fa1cee5d26750eb30460
EgmTrg <e.turoglu@hotmail.com>
2021-06-14 11:56:20 +0300

    New: Created Entity.Stock Class

---
+++ Muhasebe/Muhasebe.Entity/Stock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Muhasebe.Entity
+{
+    class Stock : TableObjects
+    {
+        public override string PrimaryKey => "ProductID";
+        public int GroupNo { get; set; }
+        public int StockCode { get; set; }
+        public string Barcode { get; set; }
+        public string Name { get; set; }
+        public int Quantity_In { get; set; }
+        public int Quantity_Out { get; set; }
+        public int Quantity_Remaining { get; set; }
+        public bool Status { get; set; }
+        public decimal Price_Buy { get; set; }
+        public decimal Price_Payee { get; set; }
+        public decimal Price_Reatil { get; set; }
+    }
+}
